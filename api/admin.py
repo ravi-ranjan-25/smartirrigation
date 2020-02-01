7ca65be389b55fd62dbdf549839a9463576c5306
Your Name <raviranjanv25@gmail.com>
2020-02-01 19:33:43 +0530

    ii

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from api.models import Grid,weather,fieldParameter
+from api.models import Grid,weather,fieldParameter,userDetails
 # Register your models here.
 admin.site.register(Grid)
 admin.site.register(weather)
 admin.site.register(fieldParameter)
+admin.site.register(userDetails)
 
 
