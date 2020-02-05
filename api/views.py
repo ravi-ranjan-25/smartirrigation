84b35f5b0e11319c41eb021f7b9c2e7a4bcff818
Your Name <raviranjanv25@gmail.com>
2020-02-06 05:14:45 +0530

    ini

--- api/views.py
+++ api/views.py
@@ -82,7 +82,7 @@ def gridCall(request):
 
 def gridCallSoftware(request):
     
-    w = Grid.objects.latest('time')
+    w = Grid.objects.filter('time')[0]
     z = weather.objects.latest('time')
     
 
